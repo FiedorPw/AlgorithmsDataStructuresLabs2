2ef9012bc7a97560727ef5fade3f228581337865
Fiedor <01168927@pw.edu.pl>
2022-04-06 16:47:29 +0200

    działające sprawdzanie podzielnosći

--- Lab3/DividingD.java
+++ Lab3/DividingD.java
@@ -1,27 +1,48 @@
 package Lab3;
 
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.LinkedList;
+import java.util.*;
 
 public class DividingD {
     public static void main(String[] args){
-        Integer[] lista = {1,2,3,4,5,6};
+        int[] lista = {1,2,3,4,5,6,3,3,4,4};
         DividingD divide = new DividingD();
-        divide.checkD(lista,3);
+        System.out.println(divide.checkD(lista,7));
     }
-    public boolean checkD(Integer[] array,int d){
+    public boolean checkD(int[] array,int d){
         if(array.length%2 ==1)return false;
+
+        HashMap<Integer, Integer> hashMap = new HashMap<>();
+        Integer tmpvalue = 0;
+        Integer moduloresult = 0;
         for (int i = 0; i < array.length; i++) {
-            array[i] = array[i]%d;
-
+            moduloresult = array[i]%d;
+            if(hashMap.get(moduloresult) != null) {
+                tmpvalue = hashMap.get(moduloresult);
+            }else{
+                tmpvalue = 0;
+            }
+            hashMap.put( moduloresult ,tmpvalue +1);
         }
-        LinkedList<Integer> linkedList = new LinkedList<Integer>();
-        Collections.addAll(linkedList, array);
 
-        System.out.println(linkedList);
+        if(hashMap.get(0) != null && hashMap.get(0)%2 ==1){
+            return false;
+        }else {
+            hashMap.remove(0);
+        }
 
+        System.out.println(hashMap);
+
+        int k = d-1;
+        for (int i = 1; i < d/2+1; i++) {
+            if(!hashMap.get(i).equals(hashMap.get(k))){
+                return false;
+            }else if(i == k && hashMap.get(i)%2 == 1) {
+                return false;
+            }else{
+
+                k--;
+            }
+        }
 
         return true;
     }
